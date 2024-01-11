286ec419f5dbbfbfe44889b9bf5579b8e755fc19
ianmacdougald <35744006+ianmacdougald@users.noreply.github.com>
2024-01-11 10:56:43 -0500

    Modularize HeaderLeft

--- app/components/Header/HeaderLeft.tsx
+++ app/components/Header/HeaderLeft.tsx
@@ -1,75 +1,14 @@
 import React, { useState, useContext } from 'react';
-import { Fatality } from '../Sounds/Fatality';
-import { TabContext } from '../../context/TabContext';
-import { RemoveContext } from '../../context/RemoveContext';
-import { SoundContext } from '../../context/SoundContext';
-import { Tab } from '../../../types';
-import getTabs from '../../utils/getTabs';
-import { SelectContext } from '../../context/SelectContext';
+import { RandomMode } from './Modes/Random';
+import { SelectAllMode } from './Modes/SelectAll';
 
 const HeaderLeft = () => {
-  const [fatality, setFatality] = useState(false);
-  const setTabToDelete = useContext(RemoveContext)?.setTabToDelete;
-  const allTabs = useContext(TabContext)?.allTabs;
-  const setAllTabs = useContext(TabContext)?.setAllTabs;
-  const soundOn = useContext(SoundContext)?.soundOn;
-
-  const [allSelected, setAllSelected] = useState<boolean>(false);
-  const addToSelectedTabs = useContext(SelectContext)?.addToSelectedTabs;
-  const removeFromSelectedTabs = useContext(SelectContext)?.removeFromSelectedTabs;
-  
-  const randomClick = async () => {
-    if (allTabs) {
-      const tabsList = Object.values(allTabs).reduce((acc, curr): Tab[] => {
-        acc.push(...curr);
-        return acc;
-      }, []);
-      const randNum = Math.floor(Math.random() * tabsList.length);
-      const randTab = tabsList[randNum].tabId;
-
-
-      if(soundOn) setFatality(prev => !prev);
-      if (randTab) {
-        if (setTabToDelete) setTabToDelete(randTab);
-        const tabToDelete = await chrome.tabs.get(randTab);
-        const { active, windowId } = tabToDelete;
-        let timeToRemove: number;
-        if (active) timeToRemove = 1500;
-        else timeToRemove = 500;
-    
-        setTimeout(chrome.tabs.remove, timeToRemove, randTab);
-        setTimeout(() => getTabs().then(tabs => {
-          if (setAllTabs) setAllTabs(tabs);
-        }), timeToRemove + 100);
-        if(soundOn) setTimeout(() => setFatality(prev => !prev), 1500);
-      }
-    };
-  };
-
-  const selectAll = () => { 
-    if (allTabs && !allSelected) {
-        if (addToSelectedTabs) { 
-          const tabs = Object.values(allTabs)[0]
-              .filter(tab => tab.tabId)
-              .map(tab => Number(tab.tabId));
-
-          addToSelectedTabs(...tabs);
-          setAllSelected(true);
-        }
-      }
-    else if (allSelected) { 
-      setAllSelected(false);
-      // With no input, removes all from selected tabs by default
-      if (removeFromSelectedTabs) removeFromSelectedTabs();
-    }
-  }
-
   return (
     <div className='flex items-center'>
-      <button className='font-medium text-sm tooltip' id="random" data-tip='Close a tab at random' onClick={randomClick}>
-        Random
-      </button>
-      {fatality && soundOn && <Fatality play={fatality}/>}
+      <SelectAllMode></SelectAllMode>
+      <RandomMode></RandomMode>
+
+     
     </div>
   )
 }
