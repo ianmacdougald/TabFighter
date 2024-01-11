1dc9de7e380d6313deaacf3befb0d49c533c6f80
ianmacdougald <35744006+ianmacdougald@users.noreply.github.com>
2024-01-11 11:48:45 -0500

    Conditionally render delete button in HeaderLeft

--- app/components/Header/HeaderLeft.tsx
+++ app/components/Header/HeaderLeft.tsx
@@ -1,14 +1,16 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { RandomMode } from './Modes/Random';
 import { SelectAllMode } from './Modes/SelectAll';
+import { DeleteMode } from './Modes/Delete';
+import { SelectContext } from '../../context/SelectContext';
 
 const HeaderLeft = () => {
+  const selectedTabs = useContext(SelectContext)?.selectedTabs;
   return (
     <div className='flex items-center'>
+      { selectedTabs && selectedTabs.size ? <DeleteMode /> : null }
       <SelectAllMode></SelectAllMode>
       <RandomMode></RandomMode>
-
-     
     </div>
   )
 }
